00b81fb2431dffd33d7cd9ca98f8a29b6043ebc6
Thomas Szteliga <ts@websafe.pl>
2013-03-03 08:44:48 +0100

    Updated build-script for README.md: Added TODO and REQUIREMENTS.

--- scripts/build/update-readme.sh
+++ scripts/build/update-readme.sh
@@ -8,4 +8,8 @@ cat USAGE.md >> ../../README.md
 echo "" >> ../../README.md
 cat CONTRIBUTE.md >> ../../README.md
 echo "" >> ../../README.md
+cat REQUIREMENTS.md >> ../../README.md
+echo "" >> ../../README.md
+cat TODO.md >> ../../README.md
+echo "" >> ../../README.md
 cat LINKS.md >> ../../README.md
