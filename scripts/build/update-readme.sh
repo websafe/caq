f3f8235945f1f8040b84bd6733422e7cc36b13e9
Thomas Szteliga <ts@websafe.pl>
2013-03-04 23:20:42 +0100

    Updated readme build-script.

--- scripts/build/update-readme.sh
+++ scripts/build/update-readme.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 IFS="
 "
-cat ABOUT.md > ../../README.md
+echo "" > ../../README.md
+
+cat ../../source/partial-content/ABOUT.md > ../../README.md
 echo "" >> ../../README.md
 
-cat INSTALL.md >> ../../README.md
+cat ../../source/partial-content/INSTALL.md >> ../../README.md
 echo "" >> ../../README.md
 
-cat USAGE.md >> ../../README.md
+cat ../../source/partial-content/USAGE.md >> ../../README.md
 echo "" >> ../../README.md
 
-cat CONTRIBUTE.md >> ../../README.md
+cat ../../source/partial-content/CONTRIBUTE.md >> ../../README.md
 echo "" >> ../../README.md
 
-cat REQUIREMENTS.md >> ../../README.md
+cat ../../source/partial-content/REQUIREMENTS.md >> ../../README.md
 echo "" >> ../../README.md
 
-cat TODO.md >> ../../README.md
+cat ../../source/partial-content/TODO.md >> ../../README.md
 echo "" >> ../../README.md
 
-cat HOW_THIS_WORKS.md >> ../../README.md
+cat ../../source/partial-content/HOW_THIS_WORKS.md >> ../../README.md
 echo "" >> ../../README.md
 
 echo "" >> ../../README.md
-cat LICENSE.md >> ../../README.md
+cat ../../source/partial-content/LICENSE.md >> ../../README.md
 grep -E "(^# |^#$)" ../../caq.sh | cut -d'#' -f2- | cut -d' ' -f2- >> ../../README.md
 echo "" >> ../../README.md
 
@@ -31,7 +33,7 @@ echo "" >> ../../README.md
 echo "Links" >> ../../README.md
 echo "--------------------------------------------------------------------------------" >> ../../README.md
 echo "" >> ../../README.md
-for linkrow in $(grep -E "^\[" LINKS.md | sort); do
+for linkrow in $(grep -E "^\[" ../../source/partial-content/LINKS.md | sort); do
     linktitle=$(echo $linkrow | cut -d'[' -f2 | cut -d']' -f1)
     linkuri=$(echo $linkrow | cut -d':' -f2- | cut -d'"' -f1 | tr -d ' ')
     linkdescr=$(echo $linkrow | cut -d':' -f2- | cut -d'"' -f2 | grep -vE "^ ")
@@ -41,5 +43,5 @@ done
 
 echo "" >> ../../README.md
 echo "" >> ../../README.md
-cat LINKS.md >> ../../README.md
+cat ../../source/partial-content/LINKS.md >> ../../README.md
 
