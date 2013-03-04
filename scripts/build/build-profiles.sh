c82dc0335e6b6d20febbb5a6f95b97f1dc57a983
Thomas Szteliga <ts@websafe.pl>
2013-03-04 20:48:39 +0100

    Created script for compiling profiles.

---
+++ scripts/build/build-profiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Compiles profiles into a format that can be added
+# at the end of caq.sh and then parsed by bash functions.
+IFS="
+"
+for profilesource in ../../source/profiles/*.profile; do
+    profileName=$(basename $profilesource .profile)
+    profileContent=$(cat ${profilesource})
+    echo "$profileContent"  | sed \
+	-e "s/^SA:/SA:${profileName}:/g" \
+	-e "s/^PKG:/PKG:${profileName}:/g" \
+	-e "s/^/### /g"
+    #echo "$profileContent";
+    echo "###"
+    echo "###"
+done
