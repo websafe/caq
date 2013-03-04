0661ab7d725c59bea39a83f89fb5858955fcc01d
Thomas Szteliga <ts@websafe.pl>
2013-03-04 21:39:40 +0100

    New build-script.

---
+++ scripts/build/build-caq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Compiles profiles into a format that can be added
+# at the end of caq.sh and then parsed by bash functions.
+IFS="
+"
+
+OUTPUTFILE=caq.sh
+
+cat ../../source/caq-core.sh > ./${OUTPUTFILE}.tmp
+./build-profiles.sh >> ./${OUTPUTFILE}.tmp
+echo -n "### EOF" >> ./${OUTPUTFILE}.tmp
+#chmod +x ./${OUTPUTFILE}.tmp
+cat ./${OUTPUTFILE}.tmp \
+    | sed -e "s/\t/    /g" -e "s/ $//g" \
+    > ./${OUTPUTFILE}.tmp2
+chmod +x ./${OUTPUTFILE}.tmp2
+mv -v ./${OUTPUTFILE}.tmp2 ../../${OUTPUTFILE}
+rm ./${OUTPUTFILE}.tmp
